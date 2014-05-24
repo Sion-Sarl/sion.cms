5ff51e7a1b040ab94633db4f110c361ff29dead6
dartuchiwa <dartuchiwa@gmail.com>
2014-05-24 21:09:52 +1100

    Add sub modules

--- app/views/components/error.blade.php
+++
@@ -1,5 +0,0 @@
-@if(Session::has('error'))
-<div class="alert-danger alert">
-    {{{Session::get('error')}}}
-</div>
-@endif
--- app/views/components/success.blade.php
+++
@@ -1,5 +0,0 @@
-@if(Session::has('success'))
-<div class="alert-danger alert">
-    {{{Session::get('success')}}}
-</div>
-@endif
